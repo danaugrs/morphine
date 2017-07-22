ddc0ce9fa0226203f870cd5ea057098eb5ce6b6d
danaugrs <danaugrs@gmail.com>
2017-07-21 17:15:17 -0700

    Improve asymetric tests

--- asymmetric.py
+++ asymmetric.py
@@ -30,19 +30,48 @@ def encrypt(pk, b):
     return cypher
 
 
+def decrypt(sk, c):
+    """Decrypt using the normal symmetric algorithm."""
+    return symmetric.decrypt(sk, c)
+
+
 if __name__ == '__main__':
 
     sk, pk = keygen()
-    assert symmetric.decrypt(sk, encrypt(pk, 0)) == 0
-    assert symmetric.decrypt(sk, encrypt(pk, 1)) == 1
+    assert decrypt(sk, encrypt(pk, 0)) == 0
+    assert decrypt(sk, encrypt(pk, 1)) == 1
 
     from testing import consistency
+
+    T = 10**5
 
     def keygenEncryptDecrypt(b):
         """asymmetric keygen-encrypt-decrypt (bit = {0})"""
         sk, pk = keygen()
         c = encrypt(pk, b)
-        return symmetric.decrypt(sk, c)
+        return decrypt(sk, c)
 
-    consistency(keygenEncryptDecrypt, (0,), 0, 10**5)
-    consistency(keygenEncryptDecrypt, (1,), 1, 10**5)
+    consistency(keygenEncryptDecrypt, (0,), 0, T)
+    consistency(keygenEncryptDecrypt, (1,), 1, T)
+
+    def keygenEncryptSum(b1, b2):
+        """asymmetric keygen-encrypt-sum ({0}+{1})"""
+        sk, pk = keygen()
+        c1 = encrypt(pk, b1)
+        c2 = encrypt(pk, b2)
+        return decrypt(sk, c1 + c2)
+
+    consistency(keygenEncryptSum, (0,0), 0, T)
+    consistency(keygenEncryptSum, (0,1), 1, T)
+    consistency(keygenEncryptSum, (1,1), 0, T)
+
+    def keygenEncryptMult(b1, b2):
+        """asymmetric keygen-encrypt-mult ({0}*{1})"""
+        sk, pk = keygen()
+        c1 = encrypt(pk, b1)
+        c2 = encrypt(pk, b2)
+        return decrypt(sk, c1 * c2)
+
+    consistency(keygenEncryptMult, (0,0), 0, T)
+    consistency(keygenEncryptMult, (0,1), 0, T)
+    consistency(keygenEncryptMult, (1,1), 1, T)
